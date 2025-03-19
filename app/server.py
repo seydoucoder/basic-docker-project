8971a724fc6b66c6e87f8b776c6afd18c2ea091b
seydoucoder <seydoucoder@gmail.com>
2025-03-19 18:00:54 +0000

    Ajout de la configuration Docker et d'un serveur HTTP simple

---
+++ app/server.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+class
+SimpleHandler(BaseHTTPRequestHandler):
+def do_GET(self): # En-têtes de
+réponse self.send_response(200)
+ self.send_header("Content-type", "text/html")
+self.end_headers()
+ # Contenu de la réponse
+ message = "<html><body><h1>Hello,
+Docker!</h1></body></html>"
+self.wfile.write(message.encode("utf-8"))
+if __name__ ==
+"__main__": PORT =
+8000
+ server_address = ("0.0.0.0", PORT)
+ httpd = HTTPServer(server_address, SimpleHandler)
+print(f"Serving on port {PORT}...")
+httpd.serve_forever()
